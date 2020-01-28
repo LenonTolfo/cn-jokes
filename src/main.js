d8892eb4ae5ae2729d473bcad5b0c2c5c044b1ae
Lenon Tolfo <le.tolfo@yahoo.com.br>
2020-01-28 21:47:02 +0100

    Install bootstrap-vue package

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
