37a0031cc62cf9c431a127846a88a11d845d0d3a
Lenon Tolfo <le.tolfo@yahoo.com.br>
2020-01-29 10:10:02 +0100

    fix bootstrap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row text-center">
+    <div class="col-2">col 4</div>
+    <div class="col-8">col 8</div>
+    <div class="col-2">col 4</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
   }
 }
 </script>
