e1f770a5c5455176abdedad18a38469450737faf
Lenon Tolfo <le.tolfo@yahoo.com.br>
2020-01-30 09:06:55 +0100

    fix header props

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,14 +5,15 @@
     </div>
     <div class="col-8 my-4">
       <div class="input-group">
-        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+        <input type="text" class="form-control" placeholder="Search text in joke" aria-describedby="button-search">
         <div class="input-group-append">
-          <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
+          <button class="btn btn-outline-secondary" type="button" id="button-search"><i class="fas fa-search"></i>
+          </button>
         </div>
       </div>
     </div>
     <div class="col-2 my-4">
-      <i v-if="mode == 'dark'" class="fas sun fa-3x"></i>
+      <i v-if="darkMode" class="fas fa-sun fa-3x"></i>
       <i v-else class="fas fa-moon fa-3x"></i>
     </div>
   </div>
@@ -21,9 +22,9 @@
 <script>
   export default {
     name: "Header",
+    props: ['darkMode'],
     data() {
       return {
-        prop: ['mode'],
         SearchText: null,
       }
     },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <Header/>
+    <Header darkMode="true"/>
   </div>
 </template>
 
