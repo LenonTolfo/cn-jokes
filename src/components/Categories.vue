1da7de0843744e00710668f0e78f8e4716bf9994
Lenon Tolfo <le.tolfo@yahoo.com.br>
2020-02-04 18:40:52 +0100

    request categories from api and save on local storage
    #6

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -10,20 +10,37 @@
 </template>
 
 <script>
+  import axios from 'axios'
 
   export default {
     name: "Categories",
     props: ['darkMode'],
     data() {
       return {
+        error: null,
         Categories: null,
       }
-    }
-    ,mounted: function() {
-      this.axios.get(process.env.VUE_APP_SECRET + '/categories')
-        .then(function(response){
-          this.Categories = response.data;
-        }.bind(this));
+    },
+    mounted() {
+      this.loadCategories()
+    },
+    methods: {
+      loadCategories () {
+
+
+        if (localStorage.getItem('categories')){
+          this.Categories = localStorage.categories;
+        } else {
+          axios.get(process.env.VUE_APP_API + '/categories')
+            .then(response => {
+              this.error = response;
+              this.Categories = response.data;
+              localStorage.setItem('categories', JSON.stringify(response.data));
+            }).catch(error => {
+            this.error = error;
+          });
+        }
+      }
     }
   }
 </script>
