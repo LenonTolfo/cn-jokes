257f5284211cd8175bc76246315b1a621f10dacb
Lenon Tolfo <le.tolfo@yahoo.com.br>
2020-02-15 11:43:03 +0100

    create Joke component to be the body of the joke
    #7

---
+++ src/components/Joke.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container-fluid joke mt-2">
+    <div class="row my-2">
+      <div class="col-2">
+        <button class="btn btn-success">prev</button>
+      </div>
+      <div class="col-8 text-center">
+        Category
+      </div>
+      <div class="col-2">
+        <button class="btn btn-success float-right">next</button>
+      </div>
+    </div>
+    <div class="row">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed suscipit nisi, eu mattis purus. Praesent sit
+        amet pretium turpis. Phasellus pharetra metus ac ipsum placerat faucibus. Aliquam et libero commodo diam sollicitudin
+        tincidunt. Suspendisse elementum libero vel risus gravida, et tincidunt arcu sodales. Sed vel nibh ac lectus tristique
+        facilisis. Vestibulum tempus hendrerit turpis vel rutrum.
+      </p>
+      <p>
+        Nullam tempor metus ut nisl scelerisque bibendum. Phasellus neque enim, condimentum porta dignissim nec, gravida
+        eu enim. Maecenas laoreet magna quis massa accumsan ultricies. Proin hendrerit dui tellus, id ornare dolor
+        pharetra vitae. Sed vitae urna odio. Nam interdum molestie est. a mollis leo odio eget
+        lacus.
+      </p>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <button class="btn btn-warning float-right">favorite</button>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+  export default {
+    name: "Joke",
+    props: ['jokeId'],
+    data () {
+      return {
+
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="container">
     <Header darkMode="true"/>
-    <div class="col-4">
-      <Categories dark-mode=""/>
+    <div class="row">
+      <div class="col-4">
+        <Categories dark-mode=""/>
+      </div>
+      <div class="col-8">
+        <Joke joke-id="new"/>
+      </div>
     </div>
   </div>
 </template>
@@ -11,10 +16,12 @@
 // @ is an alias to /src
 import Header from "../components/Header";
 import Categories from "../components/Categories";
+import Joke from "../components/Joke";
 
 export default {
   name: 'home',
   components: {
+    Joke,
     Categories,
     Header,
   }
