d198765f314f597ed7ac1a6e8a0de95e8323df16
Lenon Tolfo <le.tolfo@yahoo.com.br>
2020-03-09 17:30:56 +0100

    install Vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios, Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    category: 'random',
+  },
+  mutations: {
+    newCategory(state, category) {
+      state.category = category
+    }
+  }
+})
 
 new Vue({
   router,
